1e86de178323f2b5d1e7350b61095a4d516e9121
AnZhizhiashvili <anzhizhiashvili@tbcbank.com.ge>
2023-01-17 02:55:38 +0400

    minor changes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const jsonServer = require("json-server");
-const auth = require("json-server-auth");
-const cors = require("cors");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const port = process.env.PORT || 3300;
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
 server.db = router.db;
 
-server.use(cors());
-server.use(auth);
+server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log("listening on http://localhost:3000");
+});
